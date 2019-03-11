ff921d126488d9c6dc315352dc6538c93fe6d89b
XY-ZHOU <731986251@qq.com>
2019-03-11 22:21:15 +0800

    class of student

--- src/main/java/company/model/Student.java
+++ src/main/java/company/model/Student.java
@@ -1,4 +1,39 @@
 package main.java.company.model;
 
+import java.util.List;
+
 public class Student {
+    private String id;
+    private String name;
+    private List<Subject> scores;
+
+    public Student(String name, String id, List<Subject> scores) {
+        this.name = name;
+        this.id = id;
+        this.scores = scores;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List getScores() {
+        return scores;
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for (Subject s : scores) {
+            sum += s.getScore();
+        }
+        return sum;
+    }
+
+    public double getAverage() {
+        return (double) getSum() / scores.size();
+    }
 }
