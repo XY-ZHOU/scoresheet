b6418df45f74f9d9d1847edb97ec09454e80947a
XY-ZHOU <731986251@qq.com>
2019-03-12 10:02:17 +0800

    show menu

---
+++ src/main/java/company/tools/Tool.java
@@ -0,0 +1,10 @@
+package main.java.company.tools;
+
+public class Tool {
+    public  void showMenu(){
+        System.out.println("1. 添加学生");
+        System.out.println("2. 生成成绩单");
+        System.out.println("3. 退出");
+        System.out.println("请输入你的选择（1～3）：");
+    }
+}
